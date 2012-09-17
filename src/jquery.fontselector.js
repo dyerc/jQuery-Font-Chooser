473d9428b69731c82c9fcfd40d2c565b728496e8
Chris Dyer <chrisdyer1@gmail.com>
2012-09-17 20:25:45 +0100

    Improved plugin layout and added selected function to retrieve selected font.

--- src/jquery.fontselector.js
+++ src/jquery.fontselector.js
@@ -4,94 +4,112 @@
  * Copyright (c) 2012 Chris Dyer
  *
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
  * conditions are met:
- * 
+ *
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
- * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+ * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
  * and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
- * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
- * EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
  * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
  */
 
-jQuery.fn.fontSelector = function(options) {
 
-	var settings = $.extend( {
-		'hide_fallbacks' : false,
-		'selected' : function(style) {},
-		'initial' : ''
-	}, options);
+(function( $ ) {
 
-	return this.each(function() {
+  var settings;
 
-		var root = $(this);
-		var ul = $(this).find('ul');
-		ul.hide();
-		var visible = false;
+  var methods = {
+    init : function(options) {
 
-		if (settings['initial'] != '')
-		{
-			if (settings['hide_fallbacks'])
-				root.find('span').html(settings['initial'].substr(0, settings['initial'].indexOf(',')));
-			else
-				root.find('span').html(settings['initial']);
+      settings = $.extend( {
+        'hide_fallbacks' : false,
+        'selected' : function(style) {},
+        'initial' : ''
+      }, options);
 
-			root.css('font-family', settings['initial']);
-		}
+      var root = this;
+      var ul = this.find('ul');
+      ul.hide();
+      var visible = false;
 
-		ul.find('li').each(function() {
-			$(this).css("font-family", $(this).text());
+      if (settings['initial'] != '')
+      {
+        if (settings['hide_fallbacks'])
+          root.find('span').html(settings['initial'].substr(0, settings['initial'].indexOf(',')));
+        else
+          root.find('span').html(settings['initial']);
 
-			if (settings['hide_fallbacks'])
-			{
-				var content = $(this).text();
-				$(this).text(content.substr(0, content.indexOf(',')));
-			}
-		});
+        root.css('font-family', settings['initial']);
+      }
 
-		ul.find('li').click(function() {
+      ul.find('li').each(function() {
+        $(this).css("font-family", $(this).text());
 
-			if (!visible)
-				return;
+        if (settings['hide_fallbacks'])
+        {
+          var content = $(this).text();
+          $(this).text(content.substr(0, content.indexOf(',')));
+        }
+      });
 
-			ul.slideUp('fast', function() {
-				visible = false;
-			});
+      ul.find('li').click(function() {
 
-			root.find('span').html( $(this).text() );
-			root.css('font-family', $(this).css('font-family'));
+        if (!visible)
+          return;
 
-			settings['selected']($(this).css('font-family'));
-		});
+        ul.slideUp('fast', function() {
+          visible = false;
+        });
 
-		$(this).click(function(event) {
+        root.find('span').html( $(this).text() );
+        root.css('font-family', $(this).css('font-family'));
 
-			if (visible)
-				return;
+        settings['selected']($(this).css('font-family'));
+      });
 
-			event.stopPropagation();
-			
-			ul.slideDown('fast', function() {
-				visible = true;
-			});
-		});
+      $(this).click(function(event) {
 
-		$('html').click(function() {
-			if (visible)
-			{
-				ul.slideUp('fast', function() {
-					visible = false;
-				});
-			}
-		})
-	});
-}
+        if (visible)
+          return;
+
+        event.stopPropagation();
+
+        ul.slideDown('fast', function() {
+          visible = true;
+        });
+      });
+
+      $('html').click(function() {
+        if (visible)
+        {
+          ul.slideUp('fast', function() {
+            visible = false;
+          });
+        }
+      })
+    },
+    selected : function() {
+      return this.css('font-family');
+    }
+  };
+
+  $.fn.fontSelector = function(method) {
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.fontSelector' );
+    }
+  }
+}) ( jQuery );
 
